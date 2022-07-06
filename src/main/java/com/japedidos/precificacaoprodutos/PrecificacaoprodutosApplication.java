aaaa3f52fc4d9e52c0ae26828e5bdc9c6a1bd660
fagnerviana <fagsantiago@gmail.com>
2022-07-06 13:44:19 -0300

    Inicializando o projeto spring

---
+++ src/main/java/com/japedidos/precificacaoprodutos/PrecificacaoprodutosApplication.java
@@ -0,0 +1,13 @@
+package com.japedidos.precificacaoprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecificacaoprodutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrecificacaoprodutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/japedidos/precificacaoprodutos/PrecificacaoprodutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.japedidos.precificacaoprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecificacaoprodutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
